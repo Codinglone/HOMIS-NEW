1fddc73aaff39cd586c376dfe30470a9a6c86181
Codinglone <codinglone@gmail.com>
2023-08-16 23:32:24 +0200

    Added images and boilerplate for Normal Order Screen

--- screens/NormalOrder.jsx
+++ screens/NormalOrder.jsx
@@ -1,19 +1,41 @@
-import { SafeAreaView, View, TouchableOpacity, StyleSheet, Text, ScrollView } from "react-native";
+import {
+  SafeAreaView,
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  ScrollView,
+  ImageBackground,
+} from "react-native";
 import { useEffect, useState } from "react";
+import food from "../assets/food.jpg";
 
 const NormalOrder = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.container}>
-        <Text>Normal Order Screen</Text>
+      <View>
+        <ImageBackground source={food} resizeMode="cover" style={styles.imageBack}>
+            <Text>FOODS & BEVERAGES</Text>
+        </ImageBackground>
+      </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        display: "flex",
-        paddingHorizontal: 10
-    }
-})
+  container: {
+    display: "flex",
+    paddingHorizontal: 10,
+  },
+  imageBack: {
+    height: 200,
+    
+  },
+  imageText: {
+    color: "#fff",
+    fontSize: 24,
+    fontWeight: 900,
+  }
+});
 
-export default NormalOrder
+export default NormalOrder;
