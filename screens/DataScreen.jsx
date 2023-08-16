c13e34056ce2d4a9ec46070fb3e803a10dfbb408
Codinglone <codinglone@gmail.com>
2023-08-16 20:36:47 +0200

    Cleaned the codebase

--- screens/DataScreen.jsx
+++ screens/DataScreen.jsx
@@ -1,101 +1,12 @@
-// import React, { useState, useEffect } from "react";
-// import { Text, View, FlatList, StyleSheet, ScrollView, Image } from "react-native";
-// import axios from "axios";
-// import { getOrders } from "../firebase";
-// const DataScreen = async () => {
-//   const [data, setData] = useState([]);
-//   const [photos, setPhotos] = useState([]);
-//   const baseUrl = "https://jsonplaceholder.typicode.com";
-
-//   const fetchUser = async () => {
-//     const url = `${baseUrl}/users`;
-//     try {
-//       const response = await axios.get(url);
-//       setData(response.data);
-//     } catch (error) {
-//       console.error("Error fetching data:", error);
-//     }
-//   };
-
-//   const fetchPhotos = async () => {
-//     const url = `${baseUrl}/photos`;
-//     const params = {
-//       _limit: 4, // Limit the number of photos fetched to 4
-//     };
-
-//     try {
-//       const response = await axios.get(url, { params });
-//       setPhotos(response.data);
-//     } catch (error) {
-//       console.error("Error fetching photos:", error);
-//     }
-//   };
-
-//   useEffect(() => {
-//     fetchUser();
-//     fetchPhotos();
-//   }, []);
-
-//   const getOrdersList = async () => {
-//     const orders = await getOrders()
-//     return orders
-//   }
-//   const orders = await getOrdersList()
-//   console.log(orders);
-//   return (
-//     <View>
-//       {data.length > 0 ? (
-//         <FlatList
-//           data={data}
-//           renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
-//           keyExtractor={(item) => item.id.toString()}
-//         />
-//       ) : (
-//         <Text>Loading...</Text>
-//       )}
-
-//       <ScrollView>
-//         {photos.length > 0 ? (
-//           photos.slice(0, 4).map((photo) => (
-//             <Image key={photo.id} source={{ uri: photo.url }} style={styles.image} />
-//           ))
-//         ) : (
-//           <Text>Loading photos...</Text>
-//         )}
-//       </ScrollView>
-//       <ScrollView>
-//         {orders.map(order => (
-//           <Text>{order.name}</Text>
-//         ))}
-//       </ScrollView>
-//     </View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   item: {
-//     fontWeight: 400,
-//     fontSize: 18,
-//     paddingVertical: 4,
-//     textAlign: "center",
-//   },
-//   image: {
-//     width: 100,
-//     height: 200,
-//   },
-// });
-
-// export default DataScreen;
-
 import React, { useState, useEffect } from "react";
 import { Text, View, FlatList, StyleSheet, ScrollView, Image } from "react-native";
 import axios from "axios";
-import { getOrders } from "../firebase"; // Assuming this import is correct
+import { getOrders } from "../firebase"; 
 
 const DataScreen = () => {
   const [data, setData] = useState([]);
   const [photos, setPhotos] = useState([]);
-  const [orders, setOrders] = useState([]); // State for orders
+  const [orders, setOrders] = useState([]); 
 
   const baseUrl = "https://jsonplaceholder.typicode.com";
 
@@ -112,7 +23,7 @@ const DataScreen = () => {
   const fetchPhotos = async () => {
     const url = `${baseUrl}/photos`;
     const params = {
-      _limit: 4, // Limit the number of photos fetched to 4
+      _limit: 4, 
     };
 
     try {
@@ -135,7 +46,7 @@ const DataScreen = () => {
   useEffect(() => {
     fetchUser();
     fetchPhotos();
-    fetchOrders(); // Fetch orders when component mounts
+    fetchOrders();
   }, []);
 
   return (
@@ -178,7 +89,7 @@ const DataScreen = () => {
 
 const styles = StyleSheet.create({
   item: {
-    fontWeight: "400", // Changed to string value
+    fontWeight: "400", 
     fontSize: 18,
     paddingVertical: 4,
     textAlign: "center",
