6d1c99a264d4ef49670e9f98a16419efdc0c4a3b
Codinglone <codinglone@gmail.com>
2023-08-16 14:46:23 +0200

    Cleaned the codebase

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button, SafeAreaView } from "react-native";
 import MyStack from "./Stack";
 
 export default function App() {
