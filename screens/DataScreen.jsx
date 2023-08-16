b67a61543eae30a8592735df64070084960ab2e9
Codinglone <codinglone@gmail.com>
2023-08-16 13:12:39 +0200

    Added correct dependencies

--- screens/DataScreen.jsx
+++ screens/DataScreen.jsx
@@ -1,3 +1,79 @@
+// import React, { useState, useEffect } from "react";
+// import { Text, View, FlatList, StyleSheet, ScrollView, Image } from "react-native";
+// import axios from "axios";
+
+// const DataScreen = () => {
+//   const [data, setData] = useState([]);
+//   const [photos, setPhotos] = useState([]);
+//   const baseUrl = "https://jsonplaceholder.typicode.com";
+
+//   const fetchUser = async () => {
+//     const url = `${baseUrl}/users`;
+//     try {
+//       const response = await axios.get(url);
+//       setData(response.data);
+//     } catch (error) {
+//       console.error("Error fetching data:", error);
+//     }
+//   };
+
+//   const fetchPhotos = async () => {
+//     const url = `${baseUrl}/photos`;
+
+//     try {
+//       const response = await axios.get(url);
+//       setPhotos(response.data);
+//     } catch (error) {
+//       console.error("Error fetching photos:", error);
+//     }
+//   };
+
+//   useEffect(() => {
+//     fetchUser();
+//     fetchPhotos();
+//   }, []);
+
+//   return (
+//     <View>
+//       {data.length > 0 ? (
+//         <FlatList
+//           data={data}
+//           renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
+//           keyExtractor={(item) => item.id.toString()}
+//         />
+//       ) : (
+//         <Text>Loading...</Text>
+//       )}
+
+//       <ScrollView>
+//         {photos.length > 0 ? (
+//           photos.map((photo) => (
+//             <Image key={photo.id} source={{ uri: photo.url }} style={styles.image} />
+//           ))
+//         ) : (
+//           <Text>Loading photos...</Text>
+//         )}
+//       </ScrollView>
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   item: {
+//     fontWeight: "400",
+//     fontSize: 18,
+//     paddingVertical: 4,
+//     textAlign: "center",
+//   },
+//   image: {
+//     width: "100%",
+//     height: 200,
+//   },
+// });
+
+// export default DataScreen;
+
+
 import React, { useState, useEffect } from "react";
 import { Text, View, FlatList, StyleSheet, ScrollView, Image } from "react-native";
 import axios from "axios";
@@ -19,9 +95,12 @@ const DataScreen = () => {
 
   const fetchPhotos = async () => {
     const url = `${baseUrl}/photos`;
+    const params = {
+      _limit: 4, // Limit the number of photos fetched to 4
+    };
 
     try {
-      const response = await axios.get(url);
+      const response = await axios.get(url, { params });
       setPhotos(response.data);
     } catch (error) {
       console.error("Error fetching photos:", error);
@@ -47,7 +126,7 @@ const DataScreen = () => {
 
       <ScrollView>
         {photos.length > 0 ? (
-          photos.map((photo) => (
+          photos.slice(0, 4).map((photo) => (
             <Image key={photo.id} source={{ uri: photo.url }} style={styles.image} />
           ))
         ) : (
